1df2308f8180050cfeceec29c53f476078f7c9dd
vamseeuxd <vamsee.uxd@gmail.com>
2023-05-03 23:31:59 +0530

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+  <div class="container-fluid pt-3">
+    <div class="row">
+      <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+        <ul class="list-group">
+          <li class="list-group-item shadow">An item</li>
+          <li class="list-group-item shadow mt-2 border-top">A second item</li>
+        </ul>
+      </div>
+    </div>
+  </div>
   `,
 })
 export class App {
